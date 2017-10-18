2cc3489395ee25b2aad2808dde58558270e25c51
Riccardo Giubilato <riccardo.giubilato@phd.unipd.it>
2017-10-18 15:24:59 +0200

    pub range data

--- src/laser_to_occupancy_grid.py
+++ src/laser_to_occupancy_grid.py
@@ -2,7 +2,8 @@
 
 import rospy
 import numpy
-from tf import TransformListener
+import math
+import tf
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import Pose
@@ -11,7 +12,7 @@ from geometry_msgs.msg import Pose
 rospy.init_node('later_to_occupancy_grid_node')
 
 # listener of transforms between the car_base_link and the world frame
-car_pose = TransformListener()
+car_pose = tf.TransformListener()
 
 # Occupancy grid message
 map_msg = OccupancyGrid()
@@ -19,13 +20,15 @@ resolution = 0.01
 width = 500
 height = 500
 
-
 # Car pose relative to world
 x_car = 0.0
 y_car = 0.0
 
+# square size of the car footprint [m]
+footprint = 0.1
+
 # Map update rate (defaulted to 10 Hz)
-rate = 10.0
+rate = 2.0
 
 # Range data
 car_range = 0.0
@@ -37,42 +40,69 @@ def set_free_cells(grid, position, size):
 	# size: 			r     radius of the footprint
 	global resolution
 
-	off_x = position[0] // resolution
-	off_y = position[1] // resolution
+	off_x = position[1] // resolution + width  // 2
+	off_y = position[0] // resolution + height // 2
+    # Car pose relative to world
+
+	rospy.loginfo("POSE: x_off: %f  y_off: %f", off_x, off_y)
 
 	# set the roi to 1: known free positions
-	for i in range(-size//2, size//2)
-		for j in range(-size//2, size//2)
-			grid[i + off_x, j + off_y] = 1
+	for i in range(-size//2, size//2):
+		for j in range(-size//2, size//2):
+			grid[int(i + off_x), int(j + off_y)] = 1
 
-def set_obstacle(grid, position, orientation, car_range):
+def set_obstacle(grid, position, orientation, position_sonar, quaternion_sonar, car_range):
 	# set the occupied cells when detecting an obstacle
 	# grid:				ndarray [width,height]
 	# position:			[x y] pose of the car
 	# orientation:      quaternion, orientation of the car
 	global resolution
 
-	off_x = position[0] // resolution
-	off_y = position[1] // resolution
+	off_x = position[1] // resolution + width  // 2
+	off_y = position[0] // resolution + height // 2
 
-	euler = tf.transformations.euler_from_quaternions(orientation)
-	rotMatrix = numpy.array([[numpy.cos(math.pi/180*euler[1]), -numpy.sin(math.pi/180*euler[1])],
-		                    [numpy.sin(math.pi/180*euler[1]),  numpy.cos(math.pi/180*euler[1])]])
+	euler = tf.transformations.euler_from_quaternion(orientation)
 
-	obstacle = numpy.dot(rotMatrix,numpy.array([car_range // resolution, 0])) + numpy.array([off_x,off_y])
+	if not car_range == 0.0:
 
-	grid[obstacle[0], obstacle[1]] = 1
+		rospy.loginfo("OBSTACLE: yaw: %f", euler[2])
+
+		rotMatrix = numpy.array([[numpy.cos(euler[2]),   numpy.sin(euler[2])],
+			                     [-numpy.sin(euler[2]),  numpy.cos(euler[2])]])
+		obstacle = numpy.dot(rotMatrix,numpy.array([0, (car_range + position_sonar[0]) // resolution])) + numpy.array([off_x,off_y])
+		rospy.loginfo("position obstacle: x:%f y:%f", obstacle[0], obstacle[1])
+
+		# set probability of occupancy to 100 and neighbour cells to 50
+		grid[obstacle[0], obstacle[1]] = int(100)
+		if  grid[int(obstacle[0]+1), int(obstacle[1])]   < int(1):
+			grid[int(obstacle[0]+1), int(obstacle[1])]   = int(50)
+		if  grid[int(obstacle[0]), 	 int(obstacle[1]+1)] < int(1):
+			grid[int(obstacle[0]),   int(obstacle[1]+1)] = int(50)
+		if  grid[int(obstacle[0]-1), int(obstacle[1])]   < int(1):
+			grid[int(obstacle[0]-1), int(obstacle[1])]   = int(50)
+		if  grid[int(obstacle[0]),   int(obstacle[1]-1)] < int(1):
+			grid[int(obstacle[0]),   int(obstacle[1]-1)] = int(50)
+
+		t = 0.5
+		i = 1
+		free_cell = numpy.dot(rotMatrix,numpy.array([0, t*i])) + numpy.array([off_x,off_y])
+		while grid[int(free_cell[0]), int(free_cell[1])] < int(1):
+			grid[int(free_cell[0]), int(free_cell[1])] = int(0)
+			free_cell = numpy.dot(rotMatrix,numpy.array([0, t*i])) + numpy.array([off_x,off_y])
+			i = i+1;
+			print "filling cell (", int(free_cell[0]), ", ", int(free_cell[1]), ")"
 
 def callback_range(msg):
 	# callback range
 	global car_range
-	car_range = msg.ranges(0)
+	car_range = msg.ranges[0]
+
 
 # Subscribers
 range_sub = rospy.Subscriber("/car/scan", LaserScan, callback_range)
 
 # Publishers
-occ_pub = rospy.Publisher("/map", OccupancyGrid, queue_size = 10)
+occ_pub = rospy.Publisher("/car/map", OccupancyGrid, queue_size = 10)
 
 
 # main function
@@ -92,27 +122,42 @@ if __name__ == '__main__':
 		height = rospy.get_param("grid_height")
 
 	# initialize grid with -1 (unknown)
-	grid = numpy.zeros(width, height)
-	grid.fill(-1)
+	grid = numpy.zeros((width, height), dtype=numpy.int)
+	grid.fill(int(-1))
 
-	rate = rospy.Rate(rate)
+	map_msg.info.origin.position.x = - width // 2 * resolution
+	map_msg.info.origin.position.y = - height // 2 * resolution
+
+	loop_rate = rospy.Rate(rate)
 
 	while not rospy.is_shutdown():
 
-		t = car_pose.getLatestCommonTime("/car_base_link", "/world")
-		position, quaternion = car_pose.lookupTransform("/car_base_link", "/world", t)
+		try:
+			t = car_pose.getLatestCommonTime("/car_base_link", "/world")
+			position, quaternion = car_pose.lookupTransform("/world", "/car_base_link", t)
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			continue
 
-		set_free_cells(grid, position, 10)
+		try:
+			t = car_pose.getLatestCommonTime("/car_base_link", "/sonar_link")
+			position_sonar, quaternion_sonar = car_pose.lookupTransform("/car_base_link", "/sonar_link", t)
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			continue
 
-		if not car_range
-			set_obstacle(grid, position, orientation, car_range)
+		set_free_cells(grid, position, int(footprint//resolution))
+
+		set_obstacle(grid, position, quaternion, position_sonar, quaternion_sonar, car_range)
 
 		map_msg.header.frame_id = 'map'
 		map_msg.header.stamp = rospy.Time.now()
 		map_msg.info.resolution = resolution
 		map_msg.info.width = width
 		map_msg.info.height = height
-		map_msg.data = grid.reshape(1, width*height)
+
+		grid_reshaped = grid.reshape(1, width*height)
+		map_msg.data = range(width*height)
+		for i in range(width*height):
+			map_msg.data[i] = grid_reshaped[0,i]
 		occ_pub.publish(map_msg)
 
-		rate.sleep()
+		loop_rate.sleep()
--- src/sonar_to_laser.py
+++ src/sonar_to_laser.py
@@ -5,24 +5,62 @@ import tf
 import math
 import numpy
 from geometry_msgs.msg import Vector3
-from sensor_msgs.msg import LaserScan
-
+from sensor_msgs.msg import LaserScan, Range
 
 rospy.init_node('sonar_to_laser_node')
+
+# buffer
+b1 = 0.0
+b2 = 0.0
+b3 = 0.0
+
 pub = rospy.Publisher('car/scan', LaserScan, queue_size=10)
+pub_range = rospy.Publisher('car/range', Range, queue_size=10)
 
 Scan_msg = LaserScan()
+Scan_msg.header.frame_id = "sonar_link"
 Scan_msg.angle_min = math.pi/4 - 0.05
 Scan_msg.angle_max = math.pi/4 + 0.05
 Scan_msg.angle_increment = 0.1
 Scan_msg.range_min = 0.05
 Scan_msg.range_max = 0.5
 
+Range_msg = Range()
+Range_msg.header.frame_id = "sonar_link"
+Range_msg.radiation_type = 0
+Range_msg.min_range = 0.05
+Range_msg.max_range = 0.60
+Range_msg.field_of_view = 0.17   # 10deg
+
 def callback(msg):
-	Scan_msg.ranges = [msg.z / 100]
-	pub.publish(Scan_msg)
-	
+
+	# buffer for rejecting false range data
+	global b1, b2, b3
+	b1 = b2
+	b2 = b3
+	b3 = msg.z / 100
+
+	# wait to have more non null ranges before publishing
+	if b1 > 0.0 and b2 > 0.0 and b3 > 0.0:
+
+		Scan_msg.ranges = [msg.z / 100]
+		Range_msg.range = msg.z / 100
+		Scan_msg.header.stamp = rospy.Time.now()
+		Range_msg.header.stamp = rospy.Time.now()
+		pub.publish(Scan_msg)
+		pub_range.publish(Range_msg)
+
+	# publish 0.0 (could be false measuremet)
+	else:
+
+		Scan_msg.ranges = [0.0]
+		Range_msg.range = 0.0
+		Scan_msg.header.stamp = rospy.Time.now()
+		Range_msg.header.stamp = rospy.Time.now()
+		pub.publish(Scan_msg)
+		pub_range.publish(Range_msg)
+
+
 sub = rospy.Subscriber('car/power', Vector3, callback)
 
 rospy.spin()
-	
