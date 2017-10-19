38be7f0a22f6121b9e17a57e8ad41c6da4fd2bc0
Riccardo Giubilato <riccardo.giubilato@phd.unipd.it>
2017-10-19 11:12:28 +0200

    speedup

--- src/laser_to_occupancy_grid.py
+++ src/laser_to_occupancy_grid.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# OccupancyGrid publisher
+# author: Riccardo Giubilato
+# mail: riccardo.giubilato@gmail.com
+# year: 2017
+# ============================================
+# subscribed topics:
+#	/car/scan - contains LaserScan messages
+# published topics:
+#   /car/map - contains OccupancyGrid messages
+# listened tf:
+#   /car_base_link to /world
+#	/sonar_link to /car_base_link
+#   /map to /world
+# ============================================
+
 import rospy
 import numpy
 import math
@@ -14,21 +29,22 @@ rospy.init_node('later_to_occupancy_grid_node')
 # listener of transforms between the car_base_link and the world frame
 car_pose = tf.TransformListener()
 
-# Occupancy grid message
+# Initialize occupancy grid message
 map_msg = OccupancyGrid()
+map_msg.header.frame_id = 'map'
 resolution = 0.01
 width = 500
 height = 500
 
-# Car pose relative to world
+# Initialize car pose relative to world
 x_car = 0.0
 y_car = 0.0
 
 # square size of the car footprint [m]
 footprint = 0.1
 
-# Map update rate (defaulted to 10 Hz)
-rate = 2.0
+# Map update rate (defaulted to 5 Hz)
+rate = 5.0
 
 # Range data
 car_range = 0.0
@@ -42,9 +58,6 @@ def set_free_cells(grid, position, size):
 
 	off_x = position[1] // resolution + width  // 2
 	off_y = position[0] // resolution + height // 2
-    # Car pose relative to world
-
-	rospy.loginfo("POSE: x_off: %f  y_off: %f", off_x, off_y)
 
 	# set the roi to 1: known free positions
 	for i in range(-size//2, size//2):
@@ -65,15 +78,14 @@ def set_obstacle(grid, position, orientation, position_sonar, quaternion_sonar, 
 
 	if not car_range == 0.0:
 
-		rospy.loginfo("OBSTACLE: yaw: %f", euler[2])
-
 		rotMatrix = numpy.array([[numpy.cos(euler[2]),   numpy.sin(euler[2])],
 			                     [-numpy.sin(euler[2]),  numpy.cos(euler[2])]])
 		obstacle = numpy.dot(rotMatrix,numpy.array([0, (car_range + position_sonar[0]) // resolution])) + numpy.array([off_x,off_y])
-		rospy.loginfo("position obstacle: x:%f y:%f", obstacle[0], obstacle[1])
+
+		rospy.loginfo("FOUND OBSTACLE AT: x:%f y:%f", obstacle[0], obstacle[1])
 
 		# set probability of occupancy to 100 and neighbour cells to 50
-		grid[obstacle[0], obstacle[1]] = int(100)
+		grid[int(obstacle[0]), int(obstacle[1])] = int(100)
 		if  grid[int(obstacle[0]+1), int(obstacle[1])]   < int(1):
 			grid[int(obstacle[0]+1), int(obstacle[1])]   = int(50)
 		if  grid[int(obstacle[0]), 	 int(obstacle[1]+1)] < int(1):
@@ -90,7 +102,6 @@ def set_obstacle(grid, position, orientation, position_sonar, quaternion_sonar, 
 			grid[int(free_cell[0]), int(free_cell[1])] = int(0)
 			free_cell = numpy.dot(rotMatrix,numpy.array([0, t*i])) + numpy.array([off_x,off_y])
 			i = i+1;
-			print "filling cell (", int(free_cell[0]), ", ", int(free_cell[1]), ")"
 
 def callback_range(msg):
 	# callback range
@@ -121,10 +132,18 @@ if __name__ == '__main__':
 	if rospy.has_param("grid_height"):
 		height = rospy.get_param("grid_height")
 
+	# fill map_msg with the parameters from launchfile
+	map_msg.info.resolution = resolution
+	map_msg.info.width = width
+	map_msg.info.height = height
+	map_msg.data = range(width*height)
+
 	# initialize grid with -1 (unknown)
-	grid = numpy.zeros((width, height), dtype=numpy.int)
+	grid = numpy.ndarray((width, height), buffer=numpy.zeros((width, height), dtype=numpy.int),
+	         dtype=numpy.int)
 	grid.fill(int(-1))
 
+	# set map origin [meters]
 	map_msg.info.origin.position.x = - width // 2 * resolution
 	map_msg.info.origin.position.y = - height // 2 * resolution
 
@@ -144,20 +163,18 @@ if __name__ == '__main__':
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			continue
 
+		# write 0 (null obstacle probability) to the free areas in grid
 		set_free_cells(grid, position, int(footprint//resolution))
 
+		# write p>0 (non-null obstacle probability) to the occupied areas in grid
 		set_obstacle(grid, position, quaternion, position_sonar, quaternion_sonar, car_range)
 
-		map_msg.header.frame_id = 'map'
+		# stamp current ros time to the message
 		map_msg.header.stamp = rospy.Time.now()
-		map_msg.info.resolution = resolution
-		map_msg.info.width = width
-		map_msg.info.height = height
 
-		grid_reshaped = grid.reshape(1, width*height)
-		map_msg.data = range(width*height)
+		# build ros map message and publish
 		for i in range(width*height):
-			map_msg.data[i] = grid_reshaped[0,i]
+			map_msg.data[i] = grid.flat[i]
 		occ_pub.publish(map_msg)
 
 		loop_rate.sleep()
